36dfb27981708f0f5f1a4d7dfd50509ab1a0d2ce
Mallory Steffes <mallorysteffes@hotmail.com>
2018-08-06 21:38:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ function ternaryCheckCity (city) {
 
 
 function switchOnCharmFromTip () {
-  // case 'generous':
-  //   return = 'Thank you so much.';
-  //   break;
-  // case 'not as generous':
-  //   return = 'Thank you.';
-  //   break;
-  // case 'anything else':
-  //   return = 'Bye.';
-  //   break;
+  case 'generous':
+    return = 'Thank you so much.';
+    break;
+  case 'not as generous':
+    return = 'Thank you.';
+    break;
+  case 'anything else':
+    return = 'Bye.';
+    break;
 }
